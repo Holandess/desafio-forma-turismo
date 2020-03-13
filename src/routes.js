5f239a43a2a013c4bb13eaba1984d892a1114b9e
filipe.holanda <filipe.holanda@alest.com.br>
2020-03-12 21:36:59 -0300

    Enable UserController methods on call

--- src/routes.js
+++ src/routes.js
@@ -3,4 +3,7 @@ const routes = require("express").Router();
 routes.get("/usuarios");
 routes.get("/usuarios:id");
 
+routes.get("/usuarios", UserController.index);
+routes.get("/usuarios/:id", UserController.usuarioById);
+
 module.exports = routes;
